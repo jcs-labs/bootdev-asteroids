a68e217c308c370cd0a0b60313d0f3acf9bcf7cd
Jas <jcscoded@gmail.com>
2026-09-02 12:36:19 -0500

    create player instance

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -10,6 +11,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
     while True:
         log_state()
@@ -20,6 +22,7 @@ def main():
 
         screen.fill("black")
         dt = clock.tick(60) / 1000
+        player.draw(screen)
         pygame.display.flip()
 
 if __name__ == "__main__":
