279e742b14ecd802e9aaed5a47342ae06b0c9288
Jas <jcscoded@gmail.com>
2026-09-02 15:08:47 -0500

    asteroid splitting

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def main():
             for s in shots:
                 if s.collides_with(a):
                     log_event("asteroid_shot")
-                    a.kill()
+                    a.split()
                     s.kill()
 
         for d in drawable:
