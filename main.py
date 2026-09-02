d9392500d54587d9a5a4fa500a47bd7ee8a3762e
Jas <jcscoded@gmail.com>
2026-09-01 19:17:49 -0500

    add pygame clock

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
-pygame.init()
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
 
     while True:
         log_state()
@@ -20,6 +19,7 @@ def main():
                 return
 
         screen.fill("black")
+        dt = clock.tick(60) / 1000
         pygame.display.flip()
 
 if __name__ == "__main__":
